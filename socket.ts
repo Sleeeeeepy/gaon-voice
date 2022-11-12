47d86dbf336559759d6412bc6547e13a7bec759d
Sleeeeeepy <adjustedmin@gmail.com>
2022-11-12 19:53:06 +0900

    refactor: Use callback instead of emitting

--- socket.ts
+++ socket.ts
@@ -6,13 +6,13 @@ import { RtpCapabilities, RtpParameters } from "mediasoup-client/lib/RtpParamete
 export function makeSocket(ctx: Context, svr: Server)  {
     // on connection
     svr.on("connection", (socket) => {
-        configureClientSideSocket(ctx, svr, socket);
+        configureServerSideSocket(ctx, svr, socket);
     });
 
     return svr;
 }
 
-function configureClientSideSocket(ctx: Context, svr: Server, sock: Socket) {
+function configureServerSideSocket(ctx: Context, svr: Server, sock: Socket) {
     let ctrl = ctx.controller;
     sock.on("disconnect", async () => {
         let token = sock.data.token;
@@ -25,51 +25,96 @@ function configureClientSideSocket(ctx: Context, svr: Server, sock: Socket) {
         let ret = await ctrl.join(roomId, userId, token);
         // svr.emit("new_user", {userId: userId});
         await sock.join(roomId);
-        sock.broadcast.emit("new_user", {userId: userId});
-        sock.emit("join", ret);
+        sock.broadcast.emit("newUser", {userId: userId});
+        sock.emit("joinRoom", ret);
         sock.data.userId = userId;
-        sock.data.token = token;
         sock.data.roomId = roomId;
     });
 
-    sock.on("userList", async (roomId: string, token: string) => {
-        let ret = ctrl.userList(roomId, token);
-        sock.emit("userList", [...ret]);
+    sock.on("userList", async (roomId: string, token: string, callback) => {
+        try {
+            let ret = [...ctrl.userList(roomId)];
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+        }
     });
 
-    sock.on("roomList", () => {
-        let ret = ctrl.roomList();
-        sock.emit("roomList", [...ret]);
+    sock.on("roomList", (callback) => {
+        let ret =[...ctrl.roomList()];
+        callback(ret);
     });
 
-    sock.on("createSendTransport", async (roomId: string, userId: number, type: any, token: string) => {
-        let ret = await ctrl.createWebRTCTransport(roomId, userId, "Send", type, token);
-        sock.emit("createSendTransport", ret);
+    sock.on("createSendTransport", async (roomId: string, userId: number, type: any, token: string, callback) => {
+        try {
+            let ret = await ctrl.createWebRTCTransport(roomId, userId, "Send", type, token);
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+            console.log(err);
+        }
     });
 
-    sock.on("closeTransport", async (roomId: string, userId: number, transportId: string, token: string) => {
-        let ret = await ctrl.closeTransport(roomId, userId, transportId, token);
-        sock.emit("closeTransport", ret);
+    sock.on("createRecvTransport", async (roomId: string, userId: number, type: any, token: string, callback) => {
+        try {
+            let ret = await ctrl.createWebRTCTransport(roomId, userId, "Recv", type, token);
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+            console.log(err);
+        }
     });
 
-    sock.on("connectTransport", async (roomId: string, userId: number, transportId: string, dtlsParameters: DtlsParameters, token: string) => {
-        let ret = await ctrl.connectWebRTCTransport(roomId, userId, transportId, dtlsParameters, token);
-        sock.emit("connectTransport", ret);
+    sock.on("closeTransport", async (roomId: string, userId: number, transportId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.closeTransport(roomId, userId, transportId, token);
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+            console.log(err);
+        }
     });
 
-    sock.on("sendTransport", async (roomId: string, userId: number, transportId: string, paused: boolean, type: any, kind: any, rtpParameters: RtpParameters, token: string) => {
-        let ret = await ctrl.send(roomId, userId, transportId, paused, type, kind, rtpParameters, token);
-        sock.emit("sendTransport", ret);
+    sock.on("connectTransport", async (roomId: string, userId: number, transportId: string, dtlsParameters: DtlsParameters, token: string, callback) => {
+        try {
+            let ret = await ctrl.connectWebRTCTransport(roomId, userId, transportId, dtlsParameters, token);
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+            console.log(err);
+        }
     });
 
-    sock.on("receiveTransport", async (roomId: string, userId: number, transportId: string, mediaPeerId: number, type, rtpCapabilities: RtpCapabilities, token: string) => {
-        let ret = await ctrl.receive(roomId, userId, transportId, mediaPeerId, type, rtpCapabilities, token);
-        sock.emit("receiveTransport", ret);
+    sock.on("sendTransport", async (roomId: string, userId: number, transportId: string, paused: boolean, type: any, kind: any, rtpParameters: RtpParameters, token: string, callback) => {
+        try {
+            let ret = await ctrl.send(roomId, userId, transportId, paused, type, kind, rtpParameters, token);
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+            console.log(err);
+        }
     });
 
-    sock.on("leave", async (roomId: string, userId: number, token: string) => {
-        let ret = await ctrl.leave(roomId, userId, token);
-        sock.emit("leave", ret);
-        sock.broadcast.emit("user_leave", {userId: userId});
+    sock.on("receiveTransport", async (roomId: string, userId: number, transportId: string, mediaPeerId: number, type, kind, rtpCapabilities: RtpCapabilities, token: string, callback) => {
+        try {
+            let ret = await ctrl.receive(roomId, userId, transportId, mediaPeerId, type, kind, rtpCapabilities, token);
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+            console.log(err);
+        }
+    });
+
+    sock.on("leave", async (roomId: string, userId: number, token: string, callback) => {
+        try {
+            let ret = await ctrl.leave(roomId, userId, token);
+            if (ret) {
+                callback(ret);
+                sock.broadcast.emit("userLeave", {userId: userId});
+            }
+        } catch (err) {
+            callback(false);
+            console.log(err);
+        }
     });
 }
