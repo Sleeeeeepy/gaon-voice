23dc5c05c01e9c27b64db4719a3cd489975c8536
Sleeeeeepy <adjustedmin@gmail.com>
2022-11-22 15:15:14 +0900

    feat: add startSend emit, producer, consumer ctrl.

--- socket.ts
+++ socket.ts
@@ -2,6 +2,7 @@ import { Context } from "./context";
 import { Server, Socket } from 'socket.io';
 import { DtlsParameters } from "mediasoup-client/lib/Transport";
 import { RtpCapabilities, RtpParameters } from "mediasoup-client/lib/RtpParameters";
+import { numberOfWorkers } from "./config";
 
 export function configureServerSideSocket(ctx: Context, svr: Server, sock: Socket) {
     let ctrl = ctx.controller;
@@ -11,6 +12,7 @@ export function configureServerSideSocket(ctx: Context, svr: Server, sock: Socke
             let userId = sock.data.userId;
             let roomId = sock.data.roomId;
             await ctrl.leave(roomId, userId, token);
+            sock.broadcast.emit("userLeave", {userId: userId});
         } catch (err) {
             console.log(err);
         }
@@ -96,6 +98,7 @@ export function configureServerSideSocket(ctx: Context, svr: Server, sock: Socke
     sock.on("sendTransport", async (roomId: string, userId: number, transportId: string, paused: boolean, type: any, kind: any, rtpParameters: RtpParameters, token: string, callback) => {
         try {
             let ret = await ctrl.send(roomId, userId, transportId, paused, type, kind, rtpParameters, token);
+            sock.emit("startSend", userId, type, kind);
             callback(ret);
         } catch (err) {
             callback({error: err});
@@ -109,6 +112,77 @@ export function configureServerSideSocket(ctx: Context, svr: Server, sock: Socke
             callback(ret);
         } catch (err) {
             callback({error: err});
+            console.log(err);
+        }
+    });
+
+    sock.on("kick", async (roomId: string, adminId: number, victimId: number, adminToken: string, callback) => {
+        try {
+            let ret = await ctrl.kick(roomId, adminId, victimId, adminToken);
+            sock.broadcast.emit("userLeave", {userId: victimId});
+            callback(ret);
+        } catch (err) {
+            callback({error: err});
+        }
+        
+    });
+
+    sock.on("resumeConsumer", async (roomId: string, userId: number, consumerId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.resumeConsumer(roomId, userId, consumerId);
+            callback({result: ret});
+        } catch (err) {
+            callback({result: false});
+            console.log(err);
+        }
+    });
+
+    sock.on("pauseConsumer", async (roomId: string, userId: number, consumerId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.pauseConsumer(roomId, userId, consumerId, token);
+            callback({result: ret});
+        } catch (err) {
+            callback({result: false});
+            console.log(err);
+        }
+    });
+
+    sock.on("closeConsumer", async (roomId: string, userId: number, consumerId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.closeConsumer(roomId, userId, consumerId, token);
+            callback({result: ret});
+        } catch (err) {
+            callback({result: false});
+            console.log(err);
+        }
+    });
+
+    sock.on("resumeProducer", async (roomId: string, userId: number, producerId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.resumeProducer(roomId, userId, producerId, token);
+            callback({result: ret});
+        } catch (err) {
+            callback({result: false});
+            console.log(err);
+        }
+    });
+
+    sock.on("pauseProducer", async (roomId: string, userId: number, producerId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.pauseProducer(roomId, userId, producerId, token);
+            callback({result: ret});
+        } catch (err) {
+            callback({result: false});
+            console.log(err);
+        }
+    });
+
+    sock.on("closeProducer", async (roomId: string, userId: number, producerId: string, token: string, callback) => {
+        try {
+            let ret = await ctrl.closeProducer(roomId, userId, producerId, token);
+            callback({result: ret});
+        } catch (err) {
+            callback({result: false});
             console.log(err);
         }
     });
